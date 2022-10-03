6e3a5353329c58cda059b1fefd27854cfd16da09
Kristian <59235451+Dopuu@users.noreply.github.com>
2022-10-03 19:18:40 +0200

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -14,32 +14,33 @@ export default function Home() {
       <Navbar />
       <div className='grid grid-cols-2 gap-4  '>
         <h1 className='text-[40px] text-black ml-[430px] font-inder font-semibold'>Statistiky</h1>
-        <div className='col-start-1 col-end-1 '>
-          <div className='flex flex-nowrap max-w-max'>
-            <Sidebar />
-            <div className='flex flex-nowrap gap-16 h-40 ml-[430px] max-w-max '>
-              <div className='bg-black w-72 text-white rounded-r-[0px] rounded-l-[15px]'>
-                <Card />
+        <div className='col-start-1 row-span-2'>
+          <div className="flex flex-col h-full">
+            <div className='flex flex-nowrap max-w-max'>
+              <Sidebar />
+              <div className='flex flex-nowrap gap-16 h-40 ml-[430px] max-w-max '>
+                <div className='bg-black w-72 text-white rounded-r-[0px] rounded-l-[15px]'>
+                  <Card />
+                </div>
+                <div className='bg-black w-72 text-white rounded-r-[0px] rounded-l-[15px] flex-1'>
+                  <Card />
+                </div>
               </div>
-              <div className='bg-black w-72 text-white rounded-r-[0px] rounded-l-[15px] flex-1'>
-                <Card />
-              </div>
+            </div>
+            <div className='w-[666px] h-[500px]  ml-[430px] mt-8'>
+              <List />
             </div>
           </div>
         </div>
         <div className='col-start-2 px-12'>
           <div className='w-[700px] h-64  ml-auto '>
-              <Graph/>
+            <Graph />
           </div>
         </div>
-        <div className='col-start-1 px-12 ml-96 mr-auto'>
-          <div className='w-[666px] h-96  ml-auto '>
-              <List/>
-          </div>
-        </div>
-        <div className='col-start-2 px-12 mt-[85px]'>
-          <div className='w-[700px] h-[290px]  ml-auto'>
-              <History/>
+
+        <div className='col-start-2 px-12 mt-[70px]'>
+          <div className='w-[700px] h-[320px]  ml-auto'>
+            <History />
           </div>
         </div>
       </div>
